834919f6793704ff73a618d99a2bffd2dfa3764d
Quentin Bahers <quentin.bahers@gmail.com>
2015-10-08 21:39:02 +0100

    Solve Contains Duplicate

---
+++ 217/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+public class ContainsDuplicate {
+    
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            else {
+                set.add(nums[i]);
+            }
+        }
+        
+        return false;
+    }
+    
+}
