b922eea02fcb7d436a5da1d137ae75922beeb4ba
Quentin Bahers <quentin.bahers@gmail.com>
2015-10-10 17:55:37 +0100

    Solve Maximum Depth of Binary Tree

--- 100/SameTree.java
+++ 100/SameTree.java
@@ -7,11 +7,14 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 public class SameTree {
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
         if (p == null || q == null) return false;
 
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+    
 }
