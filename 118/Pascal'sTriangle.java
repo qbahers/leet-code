0a29f16d9fb77f621b87af2b3d50c2b8c06dadff
Quentin Bahers <quentin.bahers@gmail.com>
2015-10-22 19:48:31 +0100

    Solve Pascal's Triangle

---
+++ 118/Pascal'sTriangle.java
@@ -0,0 +1,24 @@
+public class Pascal'sTriangle {
+    
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<Integer>();
+            
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                }
+                else {
+                    row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
+                }
+            }
+            
+            triangle.add(row);
+        }
+        
+        return triangle;
+    }
+    
+}
