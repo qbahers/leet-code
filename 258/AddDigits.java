8fd4d5a2e1e39b6733d66cc844a9e00b14d93461
Quentin Bahers <quentin.bahers@gmail.com>
2015-11-08 12:08:18 +0000

    Improve Add Digits

--- 258/AddDigits.java
+++ 258/AddDigits.java
@@ -1,7 +1,35 @@
+/**
+ * Time:  O(1)
+ * Space: O(1)
+ */
+
 public class AddDigits {
     
     public int addDigits(int num) {
-        return num < 10 ? num : (num - 1) % 9 + 1;    
+        return (num - 1) % 9 + 1;
+    }
+    
+}
+
+/**
+ * A less efficient solution
+ */
+
+public class AddDigits {
+    
+    public int addDigits(int num) {
+        while (num > 9) {
+            int sum = 0;
+            
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            
+            num = sum;
+        }
+        
+        return num;
     }
     
 }
