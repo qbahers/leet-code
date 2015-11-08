0097e6efb74699dfb2b8146a718b450c5d6483ea
Quentin Bahers <quentin.bahers@gmail.com>
2015-11-08 12:07:21 +0000

    Improve Nim Game

--- 292/NimGame.java
+++ 292/NimGame.java
@@ -1,7 +1,12 @@
+/**
+ * Time:  O(1)
+ * Space: O(1)
+ */
+
 public class NimGame {
     
     public boolean canWinNim(int n) {
-        return n % 4 != 0;    
+        return n % 4 != 0;
     }
     
 }
