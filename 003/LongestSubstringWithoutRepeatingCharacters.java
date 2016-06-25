25e47f2e4656f95b582332dba7ae40495cdc2587
Quentin Bahers <quentin.bahers@gmail.com>
2016-06-25 15:11:59 +0100

    Improve Longest Substring Without Repeating Characters

--- 003/LongestSubstringWithoutRepeatingCharacters.java
+++ 003/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,17 +1,21 @@
+/**
+ * Time:  O(n)
+ * Space: O(1)
+ */
+
 public class LongestSubstringWithoutRepeatingCharacters {
     
     public int lengthOfLongestSubstring(String s) {
         int res = 0;
-        int start = 0;
+        int j = 0;
+        
         int[] flag = new int[256];
         Arrays.fill(flag, -1);
         
         for (int i = 0; i < s.length(); i++) {
-            if (flag[s.charAt(i) - '\0'] >= start) {
-                start = flag[s.charAt(i) - '\0'] + 1;
-            }
-            res = Math.max(res, i - start + 1);
-            flag[s.charAt(i) - '\0'] = i;
+            j = Math.max(j, flag[s.charAt(i)] + 1);
+            res = Math.max(res, i - j + 1);
+            flag[s.charAt(i)] = i;
         }
         
         return res;
