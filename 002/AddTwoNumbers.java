2ffd568978b2e2121913677d55b5391f65e05ccf
Quentin Bahers <quentin.bahers@gmail.com>
2016-06-25 15:10:51 +0100

    Improve Add Two Numbers

--- 002/AddTwoNumbers.java
+++ 002/AddTwoNumbers.java
@@ -7,36 +7,39 @@
  * }
  */
 
+/**
+ * Time:  O(n)
+ * Space: O(n)
+ */
+
 public class AddTwoNumbers {
     
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode res = new ListNode(0);
-        ListNode cur = res;
-        int carry = 0;
+        ListNode res = new ListNode(-1);
+        ListNode p = res;
         
+        int carry = 0;
         while (l1 != null || l2 != null || carry != 0) {
-            int sum = carry;
+            int digit = carry;
             
             if (l1 != null) {
-                sum += l1.val;
+                digit += l1.val;
                 l1 = l1.next;
             }
             
             if (l2 != null) {
-                sum += l2.val;
+                digit += l2.val;
                 l2 = l2.next;
             }
-                
-            cur.val = sum % 10;
-            carry = sum / 10;
             
-            if (l1 != null || l2 != null || carry != 0) {
-                cur.next = new ListNode(0);
-                cur = cur.next;
-            }
+            carry = digit / 10;
+            digit = digit % 10;
+            
+            p.next = new ListNode(digit);
+            p = p.next;
         }
         
-        return res; 
+        return res.next;
     }
     
 }
