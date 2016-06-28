f69d7f7234a2462cf1322031b15cc4a7089e3768
Quentin Bahers <quentin.bahers@gmail.com>
2016-06-28 17:47:16 +0100

    Improve Container With Most Water

---
+++ 011/ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+/**
+ * Time:  O(n)
+ * Space: O(1)
+ */
+
+public class ContainerWithMostWater {
+    
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        
+        int res = 0;
+        
+        while (i < j) {
+            res = Math.max(res, (j - i) * Math.min(height[i], height[j]));
+            
+            if (height[i] < height[j]) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        
+        return res;
+    }
+    
+}
