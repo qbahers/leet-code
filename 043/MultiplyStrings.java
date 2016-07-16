47a759d9727913a189c55f74219ed1142ec8d0e9
Quentin Bahers <quentin.bahers@gmail.com>
2016-07-16 09:45:47 +0100

    Improve Multiply Strings

--- 043/MultiplyStrings.java
+++ 043/MultiplyStrings.java
@@ -1,23 +1,28 @@
+/**
+ * Time:  O(m * n)
+ * Space: O(m + n)
+ */
+
 public class MultiplyStrings {
     
     public String multiply(String num1, String num2) {
-        int N1 = num1.length();
-        int N2 = num2.length();
+        int M = num1.length();
+        int N = num2.length();
         
-        int[] a = new int[N1 + N2];
-        for (int i = 0; i < N1; i++) {
-            for (int j = 0; j < N2; j++) {
-                a[i + j] += (num1.charAt(N1 - 1 - i) - '0') * (num2.charAt(N2 - 1 - j) - '0');
+        int[] a = new int[M + N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                a[i + j] += (num1.charAt(M - 1 - i) - '0') * (num2.charAt(N - 1 - j) - '0');
             }
         }
         
-        for (int i = 0; i < N1 + N2 - 1; i++) {
+        for (int i = 0; i < M + N - 1; i++) {
             a[i + 1] += a[i] / 10;
             a[i] %= 10;
         }
         
         StringBuilder res = new StringBuilder();
-        for (int i = N1 + N2 - 1; i >= 0; i--) {
+        for (int i = M + N - 1; i >= 0; i--) {
             if (a[i] == 0 && res.length() == 0) continue;
             
             res.append(a[i]);
